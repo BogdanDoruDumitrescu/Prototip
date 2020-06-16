9e4f415b2b8eb069e44016a26334e2ecc8073e05
BogdanDoruDumitrescu <bogdan.dumitrescu@student.upt.ro>
2020-06-16 11:30:15 +0300

    Main launch

--- src/main/java/org/fis/maven/Controllers/AdminSetPrice.java
+++ src/main/java/org/fis/maven/Controllers/AdminSetPrice.java
@@ -2,4 +2,5 @@ package org.fis.maven.Controllers;
 
 public class AdminSetPrice {
     public void setPrice(){}
+
 }
--- src/main/java/org/fis/maven/Main.java
+++ src/main/java/org/fis/maven/Main.java
@@ -2,9 +2,29 @@ package org.fis.maven;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.fis.maven.Services.AdminService;
+import org.fis.maven.Services.ClientService;
+import org.fis.maven.Services.DriverService;
 
-public class Main {
-    public static void main(String[] args){
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("AdminSetPrice.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+    public static void main (String[] args){
         Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
         LOGGER.trace("This is a TRACE log");
@@ -12,5 +32,9 @@ public class Main {
         LOGGER.info("This is an INFO log");
         LOGGER.warn("This is a WARN log");
         LOGGER.error("This is an ERROR log");
+        launch(args);
+
+
     }
+
 }
