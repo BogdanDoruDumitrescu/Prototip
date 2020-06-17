da268e963516d8a55c62946514926526db85beee
Denis Gruia <denis.gruiax@outlook.com>
2020-06-17 14:37:53 +0300

    add price.json

--- src/main/java/org/fis/maven/Controllers/AdminSetPrice.java
+++ src/main/java/org/fis/maven/Controllers/AdminSetPrice.java
@@ -1,6 +1,4 @@
 package org.fis.maven.Controllers;
-
-<<<<<<< HEAD
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -11,9 +9,9 @@ public class AdminSetPrice {
     private Label passwordLabel;
     private Label priceLabel;
 
-    private TextField usernameTextField;
-    private TextField passwordTextField;
-    private TextField priceTextField;
+    private TextField userField;
+    private TextField passwordField;
+    private TextField priceField;
 
     public void setPrice(String price){
         int foo;
@@ -33,53 +31,5 @@ public class AdminSetPrice {
     public void setPrice(){
 
     }
-
-
-=======
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.TextField;
-import org.fis.maven.Exceptions.IncorrectAmount;
-import org.fis.maven.Models.Admin;
-import org.fis.maven.Services.AdminService;
-
-import javax.xml.soap.Text;
-import java.util.ArrayList;
-
-public class AdminSetPrice {
->>>>>>> origin/SetPriceDone
-
-    @FXML
-    private TextField priceField;
-
-    @FXML
-    public void SetPrice(){
-        ArrayList<Admin> p = AdminService.getP();
-        boolean flag = false;
-
-        for(Admin i:p){
-            if(priceField.getText() == String.valueOf(0))
-                flag = true;
-        }
-
-    try
-
-    {
-        if (flag) {
-            throw new IncorrectAmount();
-        } else {
-            p.add(new Admin(Integer.parseInt(priceField.getText())));
-            AdminService.WritePrice();
-        }
-    }catch(Exception e){
-    System.out.println(e);}
-
-}
-
-}
 
 
--- src/main/java/org/fis/maven/Models/Admin.java
+++ src/main/java/org/fis/maven/Models/Admin.java
@@ -1,41 +1,24 @@
 package org.fis.maven.Models;
 
 public class Admin {
-<<<<<<< HEAD
     private static final String username = "admin";
     private static final String password = "admin";
+    private int price;
 
     public String getUsername() {
         return username;
     }
 
-    public static String getPassword() {
+    public String getPassword() {
         return password;
     }
 
+    public int getPrice(){return price;}
+    public Admin(int price){this.price = price;}
+
     public String toString() {
         return "Admin{" + "username =' " + username + '\'' +
-                ", password ='" + password + '\'' + "'}";
+                ", password ='" + password + '\'' + ", price = " + price + "'}";
     }
 }
-=======
-    private int price;
 
-    public Admin(int price){
-        this.price = price;
-    }
-
-    public Admin(){
-    }
-
-    public int getPrice() {return price;}
-
-    public void setPrice(int price) {this.price = price;}
-
-    @Override
-    public String toString() {
-            return "Admin{" + " price=" + price + " lei" +'}';
-        }
-    }
-
->>>>>>> origin/SetPriceDone
